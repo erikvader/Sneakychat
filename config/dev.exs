b414d8224cebe71e7d5c09adae1f22a7cdf9a081
Erik Rimskog <erik.rimskog@gmail.com>
2020-03-01 22:21:28 +0100

    changed port from 4000 to 80

--- config/dev.exs
+++ config/dev.exs
@@ -16,7 +16,7 @@ config :sneaky, Sneaky.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :sneaky, SneakyWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 80],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
