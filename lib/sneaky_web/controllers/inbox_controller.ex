60ef28572fcba14d07e8f7747a8d3d437a8c2124
Staffan Annerwall <staffan.aw@protonmail.com>
2020-03-02 10:32:41 +0100

    Move user_exists?, get_account to new Util module

--- lib/sneaky_web/controllers/inbox_controller.ex
+++ lib/sneaky_web/controllers/inbox_controller.ex
@@ -1,5 +1,6 @@
 defmodule SneakyWeb.InboxController do
   use SneakyWeb, :controller
+  import SneakyWeb.Util
   import Ecto.Query, only: [from: 2]
 
   # handle the receiving of a new sneak
@@ -51,28 +52,6 @@ defmodule SneakyWeb.InboxController do
         {:error, changeset} -> repo.rollback(changeset)
       end
     end)
-  end
-
-  # returns true if `username` is in our `users` table.
-  defp user_exists?(username) do
-    query = from a in Sneaky.Auth.Account,
-      join: u in Sneaky.Auth.User, on: u.account_id == a.id,
-      where: a.username == ^username and a.url == "localhost"
-    Sneaky.Repo.exists?(query)
-  end
-
-  # returns the account schema with `username` and `url`. If this
-  # account does not exists, then it is created.
-  defp get_account(username, url) do
-    alias Sneaky.Auth.Account
-
-    %Account{}
-    |> Account.changeset(%{username: username, url: url})
-    |> Sneaky.Repo.insert(on_conflict: :nothing)
-    |> case do
-         {:ok, %Account{id: id} = acc} when id != nil -> acc
-         _ -> Sneaky.Repo.get_by!(Account, [username: username, url: url])
-       end
   end
 
   # returns the sneak schema with `image_url` and `sender_acc`. If this
---
+++ lib/sneaky_web/util.ex
@@ -0,0 +1,25 @@
+defmodule SneakyWeb.Util do
+  import Ecto.Query, only: [from: 2]
+
+  # returns true if `username` is in our `users` table.
+  def user_exists?(username) do
+    query = from a in Sneaky.Auth.Account,
+      join: u in Sneaky.Auth.User, on: u.account_id == a.id,
+      where: a.username == ^username and a.url == "localhost"
+    Sneaky.Repo.exists?(query)
+  end
+
+  # returns the account schema with `username` and `url`. If this
+  # account does not exists, then it is created.
+  def get_account(username, url) do
+    alias Sneaky.Auth.Account
+
+    %Account{}
+    |> Account.changeset(%{username: username, url: url})
+    |> Sneaky.Repo.insert(on_conflict: :nothing)
+    |> case do
+         {:ok, %Account{id: id} = acc} when id != nil -> acc
+         _ -> Sneaky.Repo.get_by!(Account, [username: username, url: url])
+       end
+  end
+end
