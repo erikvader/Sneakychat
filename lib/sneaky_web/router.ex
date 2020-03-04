19b619dbe45a0fdbb70fa297bfd11932c40b494d
novium <mail@novium.pw>
2020-03-04 16:04:01 +0100

    :sparkles: Added authentication pipeline to router

--- lib/sneaky_web/router.ex
+++ lib/sneaky_web/router.ex
@@ -9,6 +9,28 @@ defmodule SneakyWeb.Router do
     plug :put_secure_browser_headers
   end
 
+  pipeline :maybe_auth do
+    plug Guardian.Plug.Pipeline, module: Sneaky.Guardian
+    plug Guardian.Plug.VerifySession, claims: %{"typ" => "access"}
+    plug Guardian.Plug.VerifyHeader, claims: %{"typ" => "access"}
+  end
+
+  pipeline :just_auth do
+    plug Guardian.Plug.EnsureAuthenticated
+  end
+
+  pipeline :load_auth do
+    plug Guardian.Plug.LoadResource, allow_blank: true
+  end
+
+  pipeline :check_admin do
+    
+  end
+
+  pipeline :check_moderator do
+
+  end
+
   pipeline :api do
     plug :accepts, ["json"]
   end
@@ -20,6 +42,12 @@ defmodule SneakyWeb.Router do
 
     get "/auth", AuthController, :request
     post "/auth/identity/callback", AuthController, :identity_callback
+
+    scope "/admin" do
+      pipe_through [:maybe_auth, :just_auth]
+
+      get "/", PageController, :index
+    end
   end
 
   scope "/api", SneakyWeb.API do
